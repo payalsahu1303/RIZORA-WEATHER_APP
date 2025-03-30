64cd7742c872bff47704f4eb37d49e779a100a94
Payal Sahu <141853271+payalsahu1303@users.noreply.github.com>
2025-03-30 12:40:27 +0530

    Add files via upload

---
+++ src/components/layout.tsx
@@ -0,0 +1,18 @@
+import type { PropsWithChildren } from "react";
+import { Header } from "./header";
+
+export function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className=" bg-gradient-to-br from-background to-muted">
+      <Header />
+      <main className="container min-h-screen px-4 py-8 mx-auto">
+        {children}
+      </main>
+      <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
+        <div className="container px-4 mx-auto text-center text-gray-200">
+          <p>Made by Payal Sahu</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
